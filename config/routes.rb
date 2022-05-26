adcb56cf70b66477dc34d2a7569dcf0743238822
mubanga <mukobam4@gmail.com>
2022-05-26 22:41:57 +0200

    added layout, markup and devise user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :posts
+  devise_for :users
+  #resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :posts, only: [:new, :create, :index]
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "posts#index"
 end
