e41dcf89e0fa46b8d554eb0f25399fcceeb8bff9
mubanga <mukobam4@gmail.com>
2022-05-26 23:14:42 +0200

    added some gems and added some fields to th user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controller => {registration: "registration"}
   #resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :posts, only: [:new, :create, :index]
