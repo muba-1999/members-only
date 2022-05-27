ac10d7246d821bfe395c26a630eb8e10bb1158e2
mubanga <mukobam4@gmail.com>
2022-05-27 12:41:10 +0200

    fixed typo in the registrations controller and added some markup

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_26_210111) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_26_213826) do
   create_table "posts", force: :cascade do |t|
     t.text "post"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
